fb37984a6f98ce927b813872b7ee8a1b150d77c8
yagami light <halysl0817@gmail.com>
2019-12-10 18:21:46 +0800

    创建一个cli app，提供install和uninstall选项

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module GinBlog
 
 go 1.13
 
-require github.com/mattn/go-sqlite3 v2.0.1+incompatible
+require (
+	github.com/mattn/go-sqlite3 v2.0.1+incompatible
+	github.com/urfave/cli/v2 v2.0.0
+)
--- main.go
+++ main.go
@@ -1,7 +1,50 @@
 package main
 
-import "GinBlog/db"
+import (
+	"GinBlog/db"
+	"fmt"
+	"github.com/urfave/cli/v2"
+	"log"
+	"os"
+)
+
+func init(){
+	log.SetFlags(log.LstdFlags|log.Lshortfile)
+}
 
 func main() {
-	db.InitDB()
+	app := &cli.App{
+		Name:  "gblog",
+		Usage: "blog deploy and remove",
+		Action: func(c *cli.Context) error {
+			fmt.Println("blog deploy and remove")
+			return nil
+		},
+		Commands: []*cli.Command{
+			{
+				Name:    "install",
+				Aliases: []string{"i"},
+				Usage:   "init blog app",
+				Action: func(c *cli.Context) error {
+					err := db.CreateDB()
+					db.InitDB()
+					return err
+				},
+			},
+			{
+				Name:    "uninstall",
+				Aliases: []string{"u"},
+				Usage:   "uninstall blog app",
+				Action: func(c *cli.Context) error {
+					err := db.RecoverEnv()
+					return err
+				},
+			},
+		},
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
 }

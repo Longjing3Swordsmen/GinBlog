fb7d0cfb925f230de2a23423771323ec8b7d3c88
yagami light <halysl0817@gmail.com>
2019-12-11 18:12:18 +0800

    增加gin app的入口

--- main.go
+++ main.go
@@ -2,10 +2,16 @@ package main
 
 import (
 	"GinBlog/db"
+	"context"
 	"fmt"
+	"github.com/gin-gonic/gin"
 	"github.com/urfave/cli/v2"
 	"log"
+	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
 func init(){
@@ -40,6 +46,14 @@ func main() {
 					return err
 				},
 			},
+			{
+				Name: "run",
+				Usage: "run blog",
+				Action: func(c *cli.Context) error {
+					startBlog()
+					return nil
+				},
+			},
 		},
 	}
 
@@ -47,4 +61,38 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func startBlog() {
+	router := gin.Default()
+	router.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Hello World",
+		})
+	})
+
+	srv := http.Server{
+		Addr: ":8000",
+		Handler: router,
+	}
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	// kill (no param) default send syscall.SIGTERM
+	// kill -2 is syscall.SIGINT
+	// kill -9 is syscall.SIGKILL but can't be catch, so don't need add it
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	log.Println("Shutdown Server ...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatal("Server Shutdown: ", err)
+	}
+	log.Println("Server exiting")
 }
